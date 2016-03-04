6caccc9bd536311fd036edce4d44257ffab29cdf
richardsamuellopez <richardsameullopez@gmail.com>
2016-03-03 21:19:15 -0500

    1st changes

--- app/js/app.js
+++ app/js/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-
+/*
 // Declare app level module which depends on filters, and services
 angular.module('myApp', [
   'ngRoute',
@@ -13,4 +13,17 @@ config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
   $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
   $routeProvider.otherwise({redirectTo: '/view1'});
+}]);
+*/
+
+angular.module('F1FeederApp', [
+  'F1FeederApp.controllers',
+  'F1FeederApp.services',
+  'ngRoute'
+]).
+config(['$routeProvider', function($routeProvider){
+  $routeProvider.
+  	when("/drivers", {templateUrl: "partials/drivers.html", controller: "driversController"}).
+  	when("/drivers/:id", {templateUrl: "partials/driver.html", controller: "driverController"}).
+  	otherwise({redirectTo: '/drivers'});
 }]);
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,7 +1,7 @@
 'use strict';
 
 /* Controllers */
-
+/*
 angular.module('myApp.controllers', [])
   .controller('MyCtrl1', ['$scope', function($scope) {
 
@@ -9,3 +9,34 @@ angular.module('myApp.controllers', [])
   .controller('MyCtrl2', ['$scope', function($scope) {
 
   }]);
+  */ 
+angular.module('F1FeederApp.controllers', []).
+
+  /* Drivers controller */
+  controller('driversController', function($scope, ergastAPIservice) {
+    $scope.nameFilter = null;
+    $scope.driversList = [];
+    $scope.searchFilter = function (driver) {
+    	var keyword = new RegExp($scope.nameFilter, 'i');
+    	return !$scope.nameFilter || keyword.test(driver.Driver.givenName) || keyword.test(driver.Driver.familyName);
+    };
+    
+    ergastAPIservice.getDrivers().success(function(response){
+    	$scope.driversList = response.MRData.StandingsTable.StandingsLists[0].DriverStandings;
+    });
+  }).
+    
+  /* Driver controller */
+  controller('driverController', function($scope, $routeParams, ergastAPIservice) {
+    $scope.id = $routeParams.id;
+    $scope.races = [];
+    $scope.driver = null;
+    
+    ergastAPIservice.getDriverDetails($scope.id).success(function(response){
+    	$scope.driver = response.MRData.StandingsTable.StandingsLists[0].DriverStandings[0];
+    });
+    
+    ergastAPIservice.getDriverRaces($scope.id).success(function(response){
+    	$scope.races = response.MRData.RaceTable.Races;
+    });  	
+  });
